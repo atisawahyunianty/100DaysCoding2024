d4ae2d8b4a8dbb382e10a9d7ff433c4a31e6007d
atisawahyunianty <atisawahyu@gmail.com>
2024-09-03 18:08:16 +0700

    Day2_coding.java

---
+++ Day2_coding.java
@@ -0,0 +1,17 @@
+package Coding100Hari;
+public class Hari2 {
+    public static void main(String[] args) {
+        
+        String nama = "Atisa Wahyunianti";
+        int tinggi = 165;
+        int berat = 51;
+        
+        //printf
+        System.out.printf(" Nama: %s \n", nama);
+        System.out.printf(" Tinggi: %d cm \n", tinggi);
+        System.out.printf(" Berat: %d kg \n", berat);
+        
+        
+    }
+    
+}
