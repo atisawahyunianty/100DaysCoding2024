c55d73620a27788a293ea111d66054a67c29d8c6
atisawahyunianty <atisawahyu@gmail.com>
2024-09-04 15:00:51 +0700

    Day3_coding.java

---
+++ Day3_coding.java
@@ -0,0 +1,22 @@
+package Coding100Hari;
+public class Hari3 {
+    public static void main(String[] args) {
+        
+        System.out.println("+----------------+-------------+-------------------+");
+        System.out.println("|Nama Husbu      |Jenis Kelamin| Dari Anime        |");
+        System.out.println("+----------------+-------------+-------------------+");
+        
+        System.out.printf("| %-14s | %-11s | %-17s |%n", "Rin Itoshi", " Laki-Laki", "Blue Lock");
+        System.out.printf("| %-14s | %-11s | %-17s |%n", "Levi Ackerman", " Laki-Laki", "Attack on Titan");
+        System.out.printf("| %-14s | %-11s | %-17s |%n", "Inoshuke", " Laki-Laki", "Kimetsu no Yaiba");
+        System.out.printf("| %-14s | %-11s | %-17s |%n", "Kaneki Ken", " Laki-Laki", "Tokyo Ghoul");
+        System.out.printf("| %-14s | %-11s | %-17s |%n", "Gojo Satoru", " Laki-Laki", "Jujutsu Kaiseng");
+        System.out.printf("| %-14s | %-11s | %-17s |%n", "Ryomen Sukuna", " Laki-Laki", "Jujutsu Kaiseng");
+        System.out.printf("| %-14s | %-11s | %-17s |%n", "Tanaka Kou", " Laki-Laki", "Ao Haru Ride");
+        
+        System.out.println("+----------------+-------------+-------------------+");
+        
+        
+    }
+    
+}
