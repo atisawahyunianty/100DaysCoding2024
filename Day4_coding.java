c0712a795eb75bcc3a326f97db2e97b9ec0c1b81
atisawahyunianty <atisawahyu@gmail.com>
2024-09-05 19:05:52 +0700

    Day4_coding.java

---
+++ Day4_coding.java
@@ -0,0 +1,13 @@
+package Coding100Hari;
+public class Hari4 {
+    public static void main(String[] args) {
+       final String grup = "Enhypen";
+       final String bias = "Sunghoon";
+       final int jumlah = 7;
+       
+        System.out.println("Nama Grup: "+ grup);
+        System.out.println("Bias: " + bias);
+        System.out.println("Jumlah Anggota: " + jumlah);
+    }
+    
+}
