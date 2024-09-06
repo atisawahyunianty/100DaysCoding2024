28bc4d01aa15aa09c1f94940215f14827e138aa9
atisawahyunianty <atisawahyu@gmail.com>
2024-09-06 16:27:25 +0700

    Day5_coding.java

---
+++ Day5_coding.java
@@ -0,0 +1,20 @@
+package Coding100Hari;
+public class Hari5 {
+    public static void main(String[] args) {
+        
+        //judul novel
+        String novel1 = "Dari Hava surat keduabelas";
+        String novel2 = "7 Wishes";
+        String novel3 = "Ruang Tanpa Suara";
+        
+        //Menggunakan println
+        System.out.println("Novel favorit pertama: " + novel1);
+        
+        //Menggunakan Printf
+        System.out.printf("Novel favorit kedua: %s \n" , novel2);
+        
+        //Menggunakan print (tanpa ln)
+        System.out.print("Novel faforit ketiga: " + novel3);
+    }
+    
+}
