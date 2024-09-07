a2b6f7aca344486a4fceff9aad1e9f9b7390fdf6
atisawahyunianty <atisawahyu@gmail.com>
2024-09-07 13:37:54 +0700

    Day6_coding.java

---
+++ Day6_coding.java
@@ -0,0 +1,20 @@
+package Coding100Hari;
+public class Hari6 {
+    public static void main(String[] args) {
+        
+        //Deklarasi variabel bertipe data String
+        String anime = "Kimetsu no Yaiba"; 
+        String mayat = "Banyak";
+        
+         //Deklarasi variabel bertipe data integer
+        int jumlahSeason = 4;
+        int Movie = 1; 
+        
+        
+        System.out.println("Judul Anime: " + anime);
+        System.out.println("Jumlah Season: " + jumlahSeason);
+        System.out.println("Jumlah Movie: " + Movie);
+        System.out.printf("Memiliki mayat yang %s\n " ,mayat);
+    }
+    
+}
