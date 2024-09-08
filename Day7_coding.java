b37631a7fd7607ed76b99db566282519ce9d17d5
atisawahyunianty <atisawahyu@gmail.com>
2024-09-08 17:48:37 +0700

    Day7_coding.java

---
+++ Day7_coding.java
@@ -0,0 +1,20 @@
+package Coding100Hari;
+public class Hari7 {
+    public static void main(String[] args) {
+        
+        // Inisialisasi Variabel String
+        String nama = "Atisa Wahyunianti";
+        String prodi = "Informatika";
+        
+        //Inisialisasi Variabel Integer
+        int tb = 165;
+        int bb = 51;
+        
+        System.out.println("Nama :" + nama);
+        System.out.println("Prodi :" + prodi);
+        System.out.println("Tinggi Badan :" + tb + "cm");
+        System.out.println("Berat Badan :" + bb + "kg");
+        
+    }
+    
+}
