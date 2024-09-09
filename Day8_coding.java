e1b0f6f9578338e973d2f9bdbf474ee3438fe16e
atisawahyunianty <atisawahyu@gmail.com>
2024-09-09 16:03:32 +0700

    Day8_coding.java

---
+++ Day8_coding.java
@@ -0,0 +1,21 @@
+package Coding100Hari;
+public class Hari8 {
+    public static void main(String[] args) {
+        
+        // Variabel Update
+        
+        String nama = "Sunghoon";
+        nama = "Park Sunghoon";
+        
+        String membergrup = "Treasure";
+        membergrup = "Enhypen";
+        
+        int kelahiran = 2001;
+        kelahiran = 2002;
+        
+        System.out.println("Nama :" + nama);
+        System.out.println("Member Grup :" + membergrup);
+        System.out.println("Kelahiran :" + kelahiran);
+    }
+    
+}
