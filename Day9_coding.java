5bed72f6aa62c2a0d1e7669d68a45c4e2ca7ce03
atisawahyunianty <atisawahyu@gmail.com>
2024-09-10 13:41:00 +0700

    Day9_coding.java

---
+++ Day9_coding.java
@@ -0,0 +1,11 @@
+package Coding100Hari;
+public class Hari9 {
+    public static void main(String[] args) {
+        
+        // final Variable (tidak dapat diperbaharui nilainya)
+        final int nilai = 95;
+        System.out.println("Nilai ujian saya adalah = " + nilai);
+     
+    }
+    
+}
