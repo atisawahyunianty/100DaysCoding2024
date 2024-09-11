da5c4a9643aae48657154ad06c0bd4351044a94f
atisawahyunianty <atisawahyu@gmail.com>
2024-09-11 17:51:31 +0700

    Day10_coding.java

---
+++ Day10_coding.java
@@ -0,0 +1,16 @@
+package Coding100Hari;
+public class Hari10 {
+    public static void main(String[] args) {
+        
+        // Tipe data byte
+        
+        byte nilai1 = 80;
+        byte nilai2 = 85;
+        byte nilai3 = 90;
+        
+        System.out.println("Nilai Kimia saya = " + nilai1);
+        System.out.println("Nilai Fisika saya = " + nilai2);
+        System.out.println("Nilai Biologi saya = " + nilai3);
+    }
+    
+}
