867f9b08ff433677e7f03906325877e3fce82eb7
atisawahyunianty <atisawahyu@gmail.com>
2024-09-13 18:04:39 +0700

    Day12_coding.java

---
+++ Day12_coding.java
@@ -0,0 +1,17 @@
+package Coding100Hari;
+public class Hari12 {
+    public static void main(String[] args) {
+        
+        // Menggunakan Tipe Data Int
+        
+        int data1 = 50;
+        int data2 = 10;
+        int data3 = 40;
+        
+        
+        System.out.println("Untuk mencapai 100% :");
+        System.out.println("Kehadiran : " + data1);
+        System.out.println("Kuis : " + data2);
+        System.out.println("Tugas : " + data3);
+    }
+}
