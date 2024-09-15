8d5007d5e946962751a7fdfdbe692c2e64a9a31c
atisawahyunianty <atisawahyu@gmail.com>
2024-09-15 16:10:40 +0700

    Day14_coding.java

---
+++ Day14_coding.java
@@ -0,0 +1,20 @@
+package Coding100Hari;
+public class Hari14 {
+    public static void main(String[] args) {
+        //Rumus Mencari Luas Dan Keliling
+        //Luas = panjang x lebar
+        //Keliling = 2 x (panjang + lebar)
+        
+        //Inisialisasi Variabel
+        int panjang = 2;
+        int lebar = 2;
+        
+        // menghitung luas dan keliling persegi panjang
+        int luas = panjang * lebar;
+        int keliling = 2 * (panjang + lebar);
+        
+        System.out.println("Luas Persegi Panjang = " + luas);
+        System.out.println("Keliling Persegi Panjang = " + keliling);
+    }
+    
+}
