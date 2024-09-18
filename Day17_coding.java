1788d9841c37bf47869153635ce3f0be6621d744
atisawahyunianty <atisawahyu@gmail.com>
2024-09-18 19:37:48 +0700

    Day17_coding.java

---
+++ Day17_coding.java
@@ -0,0 +1,27 @@
+package Coding100Hari;
+public class Hari17 {
+    public static void main(String[] args) {
+        //Aritmatika penambahan, pengurangan, perkalian, pembagian
+        
+        int angka1 = 10;
+        int angka2 = 5;
+        
+        //Penambahan
+        int penambahan = angka1 + angka2;
+        
+        //pengurangan
+        int pengurangan = angka1 - angka2;
+        
+        //perkalian
+        int perkalian = angka1 * angka2;
+        
+        //pembagian
+        int pembagian = angka1 / angka2;
+        
+        System.out.println("Hasil penambahan = " + penambahan);
+        System.out.println("Hasil pengurangan = " + pengurangan);
+        System.out.println("Hasil perkalian = " + perkalian);
+        System.out.println("Hasil pembagian = " + pembagian);
+    }
+    
+}
