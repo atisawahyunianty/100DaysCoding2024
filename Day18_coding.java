fc31c6f50c2b26666901f331b72b81fc714372af
atisawahyunianty <atisawahyu@gmail.com>
2024-09-19 18:27:47 +0700

    Day18_coding.java

---
+++ Day18_coding.java
@@ -0,0 +1,13 @@
+package Coding100Hari;
+public class Hari18 {
+    public static void main(String[] args) {
+        // Sisa Bagi
+        
+        int a = 12;
+        int b = 6;
+        
+        int sisaBagi = a % b;
+        
+        System.out.println("Sisa Bagi = " + sisaBagi);
+    }
+}
