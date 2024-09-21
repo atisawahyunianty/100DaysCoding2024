4dd6e1920436f89406e7f072eb634e6e5f730aa5
atisawahyunianty <atisawahyu@gmail.com>
2024-09-21 18:45:00 +0700

    Day20_coding.java

---
+++ Day20_coding.java
@@ -0,0 +1,25 @@
+package Coding100Hari;
+
+import java.util.Scanner;
+
+public class Hari20 {
+    public static void main(String[] args) {
+        //inputan tipe data integer dan string
+        
+        Scanner tisa = new Scanner(System.in);
+        
+        System.out.println("Masukkan nama :");
+        String nama = tisa.nextLine();
+        
+        System.out.println("Masukkan alamat :");
+        String alamat = tisa.nextLine();
+        
+        System.out.println("Masukkan umur :");
+        int umur = tisa.nextInt();
+        
+        System.out.println("Masukkan tinggi badan :");
+        int tb = tisa.nextInt();
+
+    }
+    
+}
