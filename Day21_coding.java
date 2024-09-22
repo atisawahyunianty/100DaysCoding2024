fae9a8a2d4514bb53188a929eb1488aa88d4e6c6
atisawahyunianty <atisawahyu@gmail.com>
2024-09-22 18:57:00 +0700

    Day21_coding.java

---
+++ Day21_coding.java
@@ -0,0 +1,14 @@
+package Coding100Hari;
+public class Hari21 {
+    public static void main(String[] args) {
+        //Operator perbandingan : lebih besar dan lebih kecil
+        
+        //Inisialisasi variabel
+        int a = 10;
+        int b = 12;
+        
+        System.out.println("a lebih besar dari b = " + (a > b));
+        System.out.println("a lebih kecil dari b = " + (a < b));
+    }
+    
+}
