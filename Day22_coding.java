aa9fe7b12950f898a70c7d8b41d14a603b090a43
atisawahyunianty <atisawahyu@gmail.com>
2024-09-23 19:37:00 +0700

    Day22_coding.java

---
+++ Day22_coding.java
@@ -0,0 +1,18 @@
+package Coding100Hari;
+public class Hari22 {
+    public static void main(String[] args) {
+        //operator penugasan: penjumlahan dan pengurangan
+        
+        //Penjumlahan
+        int x = 10;
+        x += 10;
+        
+        //Pengurangan
+        int y = 8;
+        y -= 2;
+        
+        System.out.println("Penjumlahan : " + x);
+        System.out.println("Pengurangan : " + y);
+    }
+    
+}
