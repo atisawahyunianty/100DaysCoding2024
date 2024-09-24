7e2f89767e52c28888f4488e0be82d698662ab1c
atisawahyunianty <atisawahyu@gmail.com>
2024-09-24 18:10:10 +0700

    Day23_coding.java

---
+++ Day23_coding.java
@@ -0,0 +1,24 @@
+package Coding100Hari;
+public class Hari23 {
+    public static void main(String[] args) {
+        //operator penugasan : perkalian, pembagian, modulus
+        
+        //perkalian
+        byte a = 6;
+        a *= 2;
+        
+        //pembagian
+        int b = 50;
+        b /= 2;
+        
+        //modulus
+        short c = 13;
+        c %= 3;
+        
+        System.out.println("Hasil perkaian :" + a);
+        System.out.println("Hasil pembagian :" + b);
+        System.out.println("Hasil Modulus atau sisa bagi :" + c);
+        
+    }
+    
+}
