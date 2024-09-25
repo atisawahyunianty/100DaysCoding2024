44d3b78cc48f71644618c0942f59b9be1d4e4f80
atisawahyunianty <atisawahyu@gmail.com>
2024-09-25 20:43:04 +0700

    Day24_coding.java

---
+++ Day24_coding.java
@@ -0,0 +1,14 @@
+package Coding100Hari;
+public class Hari24 {
+    public static void main(String[] args) {
+        //konversi tipe data string ke long
+        
+        //inisialisasi dan deklarasi 
+        String a = "150";
+        //mengonversi string ke long
+        long nilaiLong = Long.parseLong(a);
+        
+        System.out.println("setelah di ubah : " + nilaiLong);
+    }
+    
+}
