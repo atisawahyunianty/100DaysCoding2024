c73b616efea46a701728bd7663f7e6f778da8e6e
atisawahyunianty <atisawahyu@gmail.com>
2024-09-29 19:54:55 +0700

    Day28_coding.java

---
+++ Day28_coding.java
@@ -0,0 +1,11 @@
+package Coding100Hari;
+public class Hari28 {
+    public static void main(String[] args) {
+        //konversi String ke Short
+        
+        String a = "234";
+        short b = Short.parseShort(a);
+        System.out.println(b);
+    }
+    
+}
