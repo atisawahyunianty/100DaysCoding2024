b21d3caee50c4b94fff3bfadc496a9a4da29ef85
atisawahyunianty <atisawahyu@gmail.com>
2024-10-02 19:12:47 +0700

    Day31_coding.java

---
+++ Day31_coding.java
@@ -0,0 +1,15 @@
+package Coding100Hari;
+public class Hari31 {
+    public static void main(String[] args) {
+        //menghitung volume balok dengan rumus Volume = panjang x lebar x tinggi
+        
+        int panjang = 4;
+        int lebar = 6;
+        int tinggi = 10;
+        
+        int volume = panjang * lebar * tinggi;
+        
+        System.out.println("Volume balok adalah = " + volume);
+    }
+    
+}
