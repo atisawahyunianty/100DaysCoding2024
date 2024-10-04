56474409b565e14bfb140005075ae100bc94a7ac
atisawahyunianty <atisawahyu@gmail.com>
2024-10-04 19:53:57 +0700

    Day33_coding.java

---
+++ Day33_coding.java
@@ -0,0 +1,9 @@
+package Coding100Hari;
+public class Hari33 {
+    public static void main(String[] args) {
+        long a = 100;
+        String b = String.valueOf(a);
+        System.out.println("Nilai setelah di ubah =" + b);
+    }
+    
+}
