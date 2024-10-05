2f97ac1e121cbb1b87df57d2cb626b13fca58eeb
atisawahyunianty <atisawahyu@gmail.com>
2024-10-05 20:38:39 +0700

    Day34_coding.java

---
+++ Day34_coding.java
@@ -0,0 +1,18 @@
+package Coding100Hari;
+
+import java.util.Scanner;
+
+public class Hari34 {
+    public static void main(String[] args) {
+        Scanner tisa = new Scanner(System.in); 
+        
+        System.out.println("Masukkan Nama:");
+        String nama = tisa.nextLine();
+        System.out.println("Masukkan Usia:");
+        int usia = tisa.nextInt();
+        System.out.println("Masukkan tinggi badan:");
+        int tb = tisa.nextInt();
+        
+    }
+    
+}
