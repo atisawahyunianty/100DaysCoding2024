6c7d48af3749eba96547b65d1552f4de271757e1
atisawahyunianty <atisawahyu@gmail.com>
2024-10-06 17:26:31 +0700

    Day35_coding.java

---
+++ Day35_coding.java
@@ -0,0 +1,19 @@
+package Coding100Hari;
+public class Hari35 {
+    public static void main(String[] args) {
+        //operator aritmatika : penjumlaan dan pengurangan 
+        
+        //penjumlaan 
+         int a = 20;
+         int b = 20;
+         int c = a + b;
+         System.out.println("Hasil penjumalahan a dan b = " + c);
+         
+         //pengurangan 
+         int d = 10;
+         int e = 5;
+         int f = d - e;
+         System.out.println("Hasil pengurangan d dan e = " + f);
+    }
+    
+}
