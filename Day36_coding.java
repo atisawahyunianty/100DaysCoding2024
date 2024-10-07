4f3ed52ece3f3723f5cd5e7f0e16ed8439b4235b
atisawahyunianty <atisawahyu@gmail.com>
2024-10-07 19:44:43 +0700

    Day36_coding.java

---
+++ Day36_coding.java
@@ -0,0 +1,22 @@
+package Coding100Hari;
+public class Hari36 {
+    public static void main(String[] args) {
+        //operator aritmatika : perkalian, pembagian, modulo
+        
+        int nilai1 = 6;
+        int nilai2 = 2;
+        
+        // perkalian 
+        int perkalian = nilai1 * nilai2;
+        System.out.println("Hasil perkalian = " + perkalian);
+        
+        //pembagian
+        int pembagian = nilai1 / nilai2;
+        System.out.println("Hasil pembagian = " + pembagian);
+        
+        //Modulo
+        int sisaBagi = nilai1 % nilai2;
+        System.out.println("Hasil sisa bagi = " + sisaBagi);
+    }
+    
+}
