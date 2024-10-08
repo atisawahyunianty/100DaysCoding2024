e09e725e878f868f1ccec044bd207da3da0b70e4
atisawahyunianty <atisawahyu@gmail.com>
2024-10-08 19:32:24 +0700

    Day37_coding.java

---
+++ Day37_coding.java
@@ -0,0 +1,25 @@
+package Coding100Hari;
+public class Hari37 {
+
+    public static void main(String[] args) {
+        //operator penugasan 
+
+        // Pengisian Nilai
+        int a = 10;
+        System.out.println("Nilai awal a: " + a);
+
+        // Pengisian dan Penambahan
+        a += 5;  // Menambahkan 5 ke a
+        System.out.println("Nilai a setelah penambahan: " + a);
+        
+        System.out.println("----------------------------------------");
+        
+        //pengisian nilai
+        int b = 12;
+        System.out.println("Nilai awal a: " + b);
+        
+        // Pengisian dan Pengurangan
+        b -= 2;  // Mengurangi 2 dari b
+        System.out.println("Nilai b setelah pengurangan: " + b);
+    }
+}
