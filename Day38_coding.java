2c61a4d244910c5e9760d0d69958fd58e674da72
atisawahyunianty <atisawahyu@gmail.com>
2024-10-09 19:06:57 +0700

    Day38_coding.java

---
+++ Day38_coding.java
@@ -0,0 +1,25 @@
+package Coding100Hari;
+public class Hari38 {
+    public static void main(String[] args) {
+        //operator penugasan 
+
+        // Pengisian Nilai
+        int a = 10;
+        System.out.println("Nilai awal a: " + a);
+
+        // Pengisian dan Perkalian
+        a *= 5;  
+        System.out.println("Nilai a setelah dikalikan : " + a);
+        
+        System.out.println("----------------------------------------");
+        
+        //pengisian nilai
+        int b = 12;
+        System.out.println("Nilai awal b: " + b);
+        
+        // Pengisian dan Pembagian
+        b /= 2;  
+        System.out.println("Nilai b setelah dibagi : " + b);
+    }
+    
+}
