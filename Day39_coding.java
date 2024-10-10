52eccbfb26b060f038b00cdb4f7749e0731c3bfd
atisawahyunianty <atisawahyu@gmail.com>
2024-10-10 17:46:31 +0700

    Day39_coding.java

---
+++ Day39_coding.java
@@ -0,0 +1,17 @@
+package Coding100Hari;
+
+public class Hari39 {
+    public static void main(String[] args) {
+       //operator penugasan 
+
+        // Pengisian Nilai
+        int a = 10;
+        System.out.println("Nilai awal a: " + a);
+
+        // Pengisian dan Sisa Bagi
+        a %= 3;  
+        System.out.println("Nilai a :" + a);
+        
+    }
+    
+}
