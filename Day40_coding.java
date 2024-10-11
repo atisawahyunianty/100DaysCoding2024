70e2d26ace333d7ad29bdaaba702cd4fcd89f99f
atisawahyunianty <atisawahyu@gmail.com>
2024-10-11 17:32:11 +0700

    Day40_coding.java

---
+++ Day40_coding.java
@@ -0,0 +1,20 @@
+
+package Coding100Hari;
+
+public class Hari40 {
+    public static void main(String[] args) {
+        //operator perbandingan
+        int nilai1 = 12;
+        int nilai2 = 2;
+        
+        nilai1 += 3;
+        nilai2 += 3;
+        
+        System.out.println("nilai1 setelah ditambah 3 =" + nilai1);
+        System.out.println("nilai2 setelah ditambah 3 =" + nilai2);
+        
+        //membandigkan nilai1 dengan nilai2
+        System.out.println(nilai1 > nilai2);
+        System.out.println(nilai1 < nilai2);
+    }  
+}
