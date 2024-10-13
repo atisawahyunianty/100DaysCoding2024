a282752442c5f6f6176863d7aa38babf80425bda
atisawahyunianty <atisawahyu@gmail.com>
2024-10-13 17:42:07 +0700

    Day42_coding.java

---
+++ Day42_coding.java
@@ -0,0 +1,14 @@
+package Coding100Hari;
+public class Hari42 {
+    public static void main(String[] args) {
+        //operasi perbandingan sama dengan dan tidak sama dengan
+        
+        int a = 12;
+        int b = 12;
+        System.out.println(a == b);
+        
+        int c = 2;
+        int d = 3;
+        System.out.println(c != d);
+    } 
+}
