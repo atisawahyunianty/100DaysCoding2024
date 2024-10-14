78fec874a1ed04f8e036d91567a3a78cdfd180d9
atisawahyunianty <atisawahyu@gmail.com>
2024-10-14 17:03:06 +0700

    Day43_soal2.java

---
+++ Day43_soal2.java
@@ -0,0 +1,31 @@
+package Hari43;
+public class Soal2 {
+    public static void main(String[] args) {
+        
+        int a = 15;
+        int b = 20;
+        System.out.println(b);
+        
+        //20 - 15
+        b -= a;
+        System.out.println(b);
+        
+        //5 X 15
+        b *= a;
+        System.out.println(b); 
+        
+        //75 - 15
+        b -= a;
+        System.out.println(b);
+        
+        //60 / 15
+        b /= a;
+        System.out.println(b);
+        
+        //4 + 15
+        b += a;
+        System.out.println(b);
+        
+    }
+    
+}
