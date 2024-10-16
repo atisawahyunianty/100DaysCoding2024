4555cc86b238cbabe6248e7a94aa920fc353cdd4
atisawahyunianty <atisawahyu@gmail.com>
2024-10-16 17:47:04 +0700

    Day45_coding.java

---
+++ Day45_coding.java
@@ -0,0 +1,20 @@
+package Coding100Hari;
+public class Hari45 {
+    public static void main(String[] args) {
+        //operator logika
+        
+        int a = 10;
+        int b = 20;
+        
+        //AND
+        boolean hasil = (a > 5 && b > 15);
+        System.out.println("Hasil :" + hasil);
+        
+        //OR
+        boolean hasil2 = (a > 5 || b < 15);
+        System.out.println("Hasil :" + hasil2);
+        
+        
+    }
+    
+}
