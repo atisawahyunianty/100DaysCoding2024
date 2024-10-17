14b98581670219cec3c330b739fbdcb6d74e5523
atisawahyunianty <atisawahyu@gmail.com>
2024-10-17 20:22:24 +0700

    Day46_coding.java

---
+++ Day46_coding.java
@@ -0,0 +1,23 @@
+package Coding100Hari;
+
+import java.util.Scanner;
+
+public class Hari46 {
+
+    public static void main(String[] args) {
+
+        Scanner tisa = new Scanner(System.in);
+
+        System.out.print("Masukkan :");
+        boolean pertama = tisa.nextBoolean();
+
+        //negasi
+        boolean negasi = !pertama;
+
+        System.out.println("Setelah di negasi :" + negasi);
+
+    }
+
+}
+    
+
