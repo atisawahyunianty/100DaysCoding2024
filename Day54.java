f0624f144b7f2b453b79e29a2214380a351677c2
atisawahyunianty <atisawahyu@gmail.com>
2024-10-25 17:59:48 +0700

    Day54.java

---
+++ Day54.java
@@ -0,0 +1,10 @@
+package Coding100Hari;
+public class Hari54 {
+    public static void main(String[] args) {
+        // Loop untuk mencetak bilangan genap dari 0 hingga 20
+        for (int i = 0; i <= 20; i +=2) {
+            System.out.println("Bilangan genap: " + i);
+        }
+    }
+}
+
