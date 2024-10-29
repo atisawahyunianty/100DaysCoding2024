8f532f799d824ba8213891c05d05611400c27b70
atisawahyunianty <atisawahyu@gmail.com>
2024-10-29 16:40:29 +0700

    Day58.java

---
+++ Day58.java
@@ -0,0 +1,14 @@
+package Coding100Hari;
+
+public class Hari58 {
+    public static void main(String[] args) {
+       int angka = 10;
+        
+        while(angka >= 1 ){
+            System.out.println(angka + " ");
+            angka --;
+        }
+        System.out.println("selesai");
+    }
+    
+}
