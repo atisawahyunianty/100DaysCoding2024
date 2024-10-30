b5074fa1d4f8799998a581cd3630fc2f7fdab85d
atisawahyunianty <atisawahyu@gmail.com>
2024-10-30 17:06:57 +0700

    Day59.java

---
+++ Day59.java
@@ -0,0 +1,11 @@
+package Coding100Hari;
+public class Hari59 {
+    public static void main(String[] args) {
+        String kata = "ANIME";
+        
+        for(char ch : kata.toCharArray()){
+            System.out.println("Kata : " + ch);
+        }
+    }
+    
+}
