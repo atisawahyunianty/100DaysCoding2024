1c6168ff51e77dd6196894ff5551c4869e549922
atisawahyunianty <atisawahyu@gmail.com>
2024-11-01 18:28:01 +0700

    Day61.java_infiniteLoop

---
+++ Day61.java
@@ -0,0 +1,13 @@
+package Coding100Hari;
+public class Hari61 {
+    public static void main(String[] args) {
+        //infinite loop
+        int a = 0;
+        while(a >= 0){
+            System.out.println("Loop");
+            a++;
+        }
+        
+    }
+    
+}
