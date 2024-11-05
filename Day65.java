e8c64b551fd7c103478fd8176cc87f5ae2f15b8d
atisawahyunianty <atisawahyu@gmail.com>
2024-11-05 18:27:06 +0700

    Day65.java nasted loop

---
+++ Day65.java
@@ -0,0 +1,17 @@
+package Coding100Hari;
+public class Hari65 {
+    public static void main(String[] args) {
+        int tisa = 3;
+        int atisa = 6;
+        
+        for (int i = 1; i <= tisa; i++) { //loop baris 
+            for (int j = 1; j <= atisa; j++) { //loop kolom
+                System.out.print( j + " ");
+                
+            }
+            System.out.println();
+            
+        }
+    }
+    
+}
