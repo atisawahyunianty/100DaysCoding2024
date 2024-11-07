e66d10fb8cbc4f8304c64df2e50e2f364fc3a806
atisawahyunianty <atisawahyu@gmail.com>
2024-11-07 18:15:14 +0700

    Create Day67.java method void

---
+++ Day67.java
@@ -0,0 +1,15 @@
+
+package Coding100Hari;
+public class Hari67 {
+    //method void tidak menembalikan nilai
+    public void pesan(){
+        System.out.println("Arkhasa Byanthara");
+}
+    public static void main(String[] args) {
+        Hari67 cetak = new Hari67();
+        
+        //pesan
+        cetak.pesan();
+     
+    }
+}
