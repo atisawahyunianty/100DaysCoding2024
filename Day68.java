faca3c2ea13f2aef448b1461a99d5d6264da8f6c
atisawahyunianty <atisawahyu@gmail.com>
2024-11-08 19:12:27 +0700

    Create Day68.java method non void

---
+++ Day68.java
@@ -0,0 +1,18 @@
+
+package Coding100Hari;
+public class Hari68 {
+    
+    //method non void
+    public double pengembalian(){
+        return 33.3;
+    }
+    
+    public static void main(String[] args) {
+        Hari68 angka = new Hari68();
+        
+        double angka1 = angka.pengembalian();
+        System.out.println("Angka yang di kembalikan :" + angka1);
+    }
+    
+    
+}
