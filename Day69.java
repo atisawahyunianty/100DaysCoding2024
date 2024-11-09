db522dac2ec5281df6f441ed5418e33cee0a5125
atisawahyunianty <atisawahyu@gmail.com>
2024-11-09 19:15:36 +0700

    Create Day69.java menggunakan parameter

---
+++ Day69.java
@@ -0,0 +1,16 @@
+
+package Coding100Hari;
+public class Hari69 {
+    public int perkalian(int angka1, int angka2){
+        return angka1 * angka2;
+        
+    }
+    public static void main(String[] args) {
+        Hari69 kali = new Hari69();
+        
+        int mengalikan = kali.perkalian(2, 2);
+        
+        System.out.println("Hasil perkalian :" + mengalikan);
+    }
+    
+}
