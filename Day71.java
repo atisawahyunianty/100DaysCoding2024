3a1142903a233bb8b584b2d880034c7c6a47a959
atisawahyunianty <atisawahyu@gmail.com>
2024-11-11 17:56:45 +0700

    Create Day71.java method static

---
+++ Day71.java
@@ -0,0 +1,18 @@
+package Coding100Hari;
+
+public class Hari71 {
+
+    public static int Penjumlahan(int angka, int angka1) {
+        return angka + angka1;
+    }
+
+    public static void main(String[] args) {
+        int angka = 2;
+        int angka1 = 1;
+
+        int hasil = Penjumlahan(angka, angka1);
+        System.out.println(hasil);
+
+    }
+
+}
