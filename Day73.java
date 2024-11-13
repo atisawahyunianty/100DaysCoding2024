0e8506401ee6a6ee0db84b225caa6910c4e4b733
atisawahyunianty <atisawahyu@gmail.com>
2024-11-13 18:20:32 +0700

    Create Day73.java string method charAt

---
+++ Day73.java
@@ -0,0 +1,19 @@
+package Coding100Hari;
+public class Hari73 {
+    public static void main(String[] args) {
+        String kata = "Atisa";
+        
+        //memanggil huruf pertama
+        char huruf1 = kata.charAt(0);
+        System.out.println("huruf pertama :" + huruf1);
+        
+        //memanggil huruf tengah
+        char huruf2 = kata.charAt(2);
+        System.out.println("huruf tengah :" + huruf2);
+        
+        //memanggil huruf terakhir
+        char huruf3 = kata.charAt(4);
+        System.out.println("huruf terakhir :" + huruf3);
+    }
+    
+}
