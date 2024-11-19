f98d067d9b70d09753281a499d3946e3953422a8
atisawahyunianty <atisawahyu@gmail.com>
2024-11-19 18:30:51 +0700

    Create Day79.java method toString

---
+++ Day79.java
@@ -0,0 +1,16 @@
+
+package Coding100Hari;
+
+public class Hari79 {
+    public static void main(String[] args) {
+        int a = 20;
+        double b = 3.0;
+        
+        String SI = Integer.toString(a);
+        System.out.println("Mengubah int ke string :" + SI);
+        
+        String SD = Double.toString(b);
+        System.out.println("Mengubah double ke string :" + SD);
+    }
+    
+}
