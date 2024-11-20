d1da79f5aa338e5d9147a3f5aa75819f0216b5cf
atisawahyunianty <atisawahyu@gmail.com>
2024-11-20 20:39:06 +0700

    Create Day80.java method trim

---
+++ Day80.java
@@ -0,0 +1,14 @@
+
+package Coding100Hari;
+
+public class Hari80 {
+    public static void main(String[] args) {
+        String kata = " Selamat datang ";
+        
+        String kata2 = kata.trim();
+        
+        System.out.println("Sebelum : '" + kata + "'" );
+        System.out.println("Sesudah : '" + kata2 + "'" );
+    }
+    
+}
