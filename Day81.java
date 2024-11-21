111096c78a8b740990fc20db9c1a0a641bbdc83f
atisawahyunianty <atisawahyu@gmail.com>
2024-11-21 17:58:42 +0700

    Create Day81.java method valueOf

---
+++ Day81.java
@@ -0,0 +1,16 @@
+
+package Coding100Hari;
+
+public class Hari81 {
+    public static void main(String[] args) {
+        int nilai1 = 123;
+        double nilai2 = 3.5;
+        
+        String ubah1 = String.valueOf(nilai1);
+        String ubah2 = String.valueOf(nilai2);
+        
+        System.out.println("Setelah di ubah :" + ubah1);
+        System.out.println("Setelah di ubah :" + ubah2);
+    }
+    
+}
