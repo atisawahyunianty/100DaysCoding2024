fc6e599353191bc85d673885cc86f4eeda6ec4c7
atisawahyunianty <atisawahyu@gmail.com>
2024-11-23 19:39:06 +0700

    Create Day83.java membuat objeck array 1 dimensi

---
+++ Day83.java
@@ -0,0 +1,16 @@
+
+package Coding100Hari;
+
+public class Hari83 {
+    public static void main(String[] args) {
+        //Deklarasi array
+        int [] array; 
+        
+        //inisialisasi array
+        array = new int[3];
+        
+        //deklarasi dan inisialisasi
+        int [] array1 = new int[3];
+    }
+    
+}
