de86c5ae287fc5edd8c638f77dbfcf69ef544062
atisawahyunianty <atisawahyu@gmail.com>
2024-11-25 19:10:07 +0700

    Create Day85.java /mengisi elemen ke array satu dimensi pada saat di inisialisasi

---
+++ Day85.java
@@ -0,0 +1,12 @@
+
+package Coding100Hari;
+
+public class Hari85 {
+    public static void main(String[] args) {
+        
+        //mengisi elemen ke array satu dimensi pada saat di inisialisasi 
+        int angka1 [] = {10,20,30,40,50};
+        int angka2 [] = {60,70,80,90,100};
+    }
+    
+}
