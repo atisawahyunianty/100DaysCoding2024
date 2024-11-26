72c526dbcef4d797194381246a823a5ab6ec175e
atisawahyunianty <atisawahyu@gmail.com>
2024-11-26 18:24:47 +0700

    Create Day86.java mencetak array satu dimendi

---
+++ Day86.java
@@ -0,0 +1,15 @@
+
+package Coding100Hari;
+
+public class Hari86 {
+    public static void main(String[] args) {
+        
+        int angka [] = {10,20,30,40,50}; 
+        int i = 0;
+        
+        while(i < angka.length){
+            System.out.println("index ke " + i + " :" + angka[i]);
+            i++;
+        }
+    }
+}
