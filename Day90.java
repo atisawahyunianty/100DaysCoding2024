0f0024f4c2927e1b42fe59cbced1048d4f4f0897
atisawahyunianty <atisawahyu@gmail.com>
2024-11-30 22:25:11 +0700

    Create Day90.java

---
+++ Day90.java
@@ -0,0 +1,15 @@
+
+package Coding100Hari;
+
+public class Hari90 {
+    public static void main(String[] args) {
+        int g = 1;
+        int faktorial = 1;
+        do{
+            faktorial *= g;
+            g++;
+        }while(g <= 5);
+        System.out.println(faktorial);
+    }
+    
+}
